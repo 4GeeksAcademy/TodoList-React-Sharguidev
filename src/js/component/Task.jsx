f0fcafd93bbfe602b339ca07b0a29c34d0c9ceec
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-20 17:00:50 +0000

    functionality complete + css complete

--- src/js/component/Task.jsx
+++ src/js/component/Task.jsx
@@ -10,7 +10,7 @@ const Task = (props) => {
         onMouseLeave={() => {
             setIsHovered(false)
         }}>
-            <p>{props.task}</p>
+            <p className="colorFont">{props.task}</p>
             {(isHovered) && <span onClick={()=>{
                 props.onRemove()
             }}>X</span>}
